f96d8b8ba5ae24e906e73ca714f322e4e90ad320
Gustavo Cruz <gustavo.cruz@serpro.gov.br>
2013-11-14 15:15:53 -0300

    updating theme

--- theme.js
+++ theme.js
@@ -51,6 +51,7 @@
 (function($) {
   // Run code
 	$( "div.block-inner-1 div.block-inner-2 div.box div.block-outer .communities-block" ).parent().parent().parent().parent().parent().addClass( "mapa_comunidades" );
+	$( "div.track_list" ).parent().parent().parent().addClass( "track-card-list-block" );
 })(jQuery);
 
 
