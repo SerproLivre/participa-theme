13e0a7021162f1420a2296b8603809d60f392c1d
Gustavo Cruz <gustavo.cruz@serpro.gov.br>
2014-01-29 16:17:43 -0300

    New layout

--- theme.js
+++ theme.js
@@ -1,7 +1,6 @@
 (function($) {
   // Run code
-	$( ".action-home-index div.block-inner-1 div.block-inner-2 div.box div.block-outer .communities-block" ).parent().parent().parent().parent().parent().addClass( "mapa_comunidades" );
-	$( ".action-home-index div.track_list" ).parent().parent().parent().addClass( "track-card-list-block" );
+ 
 })(jQuery);
 
 
